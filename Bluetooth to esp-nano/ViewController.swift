da5569e17cc7b086b1cf9793a7938def0f4111d2
Brandon Ong <brandonong22@gmail.com>
2019-10-30 18:43:53 -0500

    Initial Commit

---
+++ Bluetooth to esp-nano/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bluetooth to esp-nano
+//
+//  Created by Brandon Ong on 10/30/19.
+//  Copyright © 2019 Brandon Ong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Bluetooth to esp-nanoTests/Bluetooth_to_esp_nanoTests.swift
@@ -0,0 +1,34 @@
+//
+//  Bluetooth_to_esp_nanoTests.swift
+//  Bluetooth to esp-nanoTests
+//
+//  Created by Brandon Ong on 10/30/19.
+//  Copyright © 2019 Brandon Ong. All rights reserved.
+//
+
+import XCTest
+@testable import Bluetooth_to_esp_nano
+
+class Bluetooth_to_esp_nanoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
